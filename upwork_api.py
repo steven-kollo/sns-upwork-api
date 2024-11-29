e07a5387625e14ab0a898a227e4efbc99ac2fdb2
Steven <steven.kollo.workspace@gmail.com>
2024-11-30 00:43:27 +0400

    1

--- upwork_api.py
+++ upwork_api.py
@@ -1,12 +1,17 @@
 import os
 import hubspot
-from hubspot.crm.imports import ApiException
+from pprint import pprint
+from hubspot.crm.deals import SimplePublicObjectInputForCreate, ApiException
 
-client = hubspot.Client.create(
-    access_token=os.environ.get("HUBSPOT_KEY"))
+client = hubspot.Client.create(access_token=os.environ.get("HUBSPOT_KEY"))
 
+properties = {
+    'dealname': 'test2',
+    'dealstage': 'appointmentscheduled'
+}
+simple_public_object_input_for_create = SimplePublicObjectInputForCreate(associations=[], object_write_trace_id="string", properties=properties)
 try:
-    api_response = client.crm.imports.core_api.get_page()
-    print(api_response)
+    api_response = client.crm.deals.basic_api.create(simple_public_object_input_for_create=simple_public_object_input_for_create)
+    pprint(api_response)
 except ApiException as e:
-    print("Exception when calling core_api->get_page: %s\n" % e)
+    print("Exception when calling basic_api->create: %s\n" % e)

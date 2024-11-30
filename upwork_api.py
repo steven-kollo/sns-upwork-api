a203e7b6f57397eddaa1de13137e385758cd8560
Steven <steven.kollo.workspace@gmail.com>
2024-11-30 21:17:25 +0400

    freshsales sample

--- upwork_api.py
+++ upwork_api.py
@@ -1,27 +1,53 @@
 import requests, json, os
+
 token = os.environ.get("FRESHSALES_KEY")
+
 headers = {
     'Content-Type': 'application/json',
     'Authorization': f'Token token={token}'
 }
 
 json_data = {
-    'deal': {
-        'name': 'Sample deal',
-        'amount': 23456,
-        'tags': [
-            '10-20 con'
+    "deal": {    
+        "name": "Name",
+        "amount": 1010,
+        "tags": [
+            "Hourly",
+            "Sr",
+            "< 1 mos",
+            "WW" 
         ],
-        'custom_field': {
-            'cf_connects_required': 8
+        "custom_field": {
+            "cf_required_skills": "API;Automation;Google Sheets",
+            "cf_connects_required": 8,
+            "cf_job_url": "https://google.com",
+            "cf_client_name": "John Doe",
+            "cf_client_location": "US",
+            "cf_client_spend": 1600,
+            "cf_client_rate": 4.8,
+            "cf_client_time_zone": 1
         }
-    },
+    }
 }
 
-r = requests.post(
+deal = requests.post(
     "https://sns1-782924923744476135.myfreshworks.com/crm/sales/api/deals/", 
     json=json_data, 
     headers=headers
 )
 
-print(json.dumps(r.json(), indent=4))
+note = requests.post(
+    "https://sns1-782924923744476135.myfreshworks.com/crm/sales/api/notes/", 
+    headers=headers,
+    json={
+        "note": {
+            "targetable_type":"Deal",
+            "targetable_id":deal.json()["deal"]["id"],
+            "description":"Hello Note"
+        }
+    }
+)
+
+r = json.dumps(note.json(), indent=4)
+print(r)
+
